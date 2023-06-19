63b33de884e758eefc56e26a37ed5c32f649b523
Manel Ariapala <amanelaa@yahoo.com>
2023-06-19 16:13:07 +0200

    Build an expenses list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 5, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 27, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 13, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 5, category: "Utilities" },
+    { id: 5, description: "eee", amount: 5, category: "Utilities" },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
