2dacbb4de89a11a8602adfd8151681383a321b3e
Manel Ariapala <amanelaa@yahoo.com>
2023-06-19 20:37:05 +0200

    Integrate react hook form & zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./assets/categories";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
